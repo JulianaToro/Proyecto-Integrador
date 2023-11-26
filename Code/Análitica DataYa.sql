8149d4659ed05453c3cb9033d4cdcaa8ae9ca0a2
siortizh <93448472+siortizh@users.noreply.github.com>
2023-11-26 10:38:45 -0500

    Delete Code/Análitica DataYa.sql

--- Code/Análitica DataYa.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE analytics_db;
-USE analytics_db;
-
-CREATE TABLE AnalysisReport (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    excel_file VARCHAR(900) NOT NULL,
-    mae FLOAT NOT NULL
-);
